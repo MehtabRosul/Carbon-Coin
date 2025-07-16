c64b691d414527573fd73b9b3a92ff2ac2ed12e9
ECX Developers <dev.encryptarx@gmail.com>
2025-07-16 17:52:28 +0000

    make the header look very very proper: Let's Get Started Tell us a bit a

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,8 +9,8 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
       {children}
-    </>
+    </div>
   );
 }
