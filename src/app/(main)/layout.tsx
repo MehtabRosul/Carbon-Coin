432fe3e33273ba1c00fd5a04460a848c6213398c
ECX Developers <dev.encryptarx@gmail.com>
2025-07-16 18:23:39 +0000

    same again look in the image and the marked area now.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,7 +9,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col min-h-screen">
       {children}
     </div>
   );
