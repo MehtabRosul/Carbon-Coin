09967b04ae51e7de87481e6c5b8b80f671bb197d
ECX Developers <dev.encryptarx@gmail.com>
2025-07-23 15:19:19 +0000

    remove the text also.

--- src/app/(main)/start/page.tsx
+++ src/app/(main)/start/page.tsx
@@ -2,15 +2,10 @@
 "use client"
 
 import * as React from "react"
-import { PageHeader } from "@/components/page-header"
 
 export default function StartPage() {
   return (
     <>
-      <PageHeader
-        title="Let's Get Started"
-        description="This page is ready to be rebuilt."
-      />
       {/* The content for the start page will be added here. */}
     </>
   )
