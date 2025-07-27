ded5a037937641e3e28416583696aafa91d7776c
ECX Developers <dev.encryptarx@gmail.com>
2025-07-27 19:13:59 +0000

    4th commit

--- src/components/sdg-icons/sdg-1.tsx
+++ src/components/sdg-icons/sdg-1.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
 export const Sdg1 = () => (
-  <img src="/E-WEB-Goal-01.png" alt="" width="128" height="128" />
+  <img src="E_SDG goals_icons-individual-rgb-01.png" alt="" width="128" height="128" />
 );
--- src/components/sdg-icons/sdg-2.tsx
+++ src/components/sdg-icons/sdg-2.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
 export const Sdg2 = () => (
-  <img src="/E-WEB-Goal-02.png" alt="" width="128" height="128" />
+  <img src="E_SDG goals_icons-individual-rgb-02.png" alt="" width="128" height="128" />
 );
